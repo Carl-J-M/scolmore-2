feedfe4d549e50ec4f3bacc16497fc8356d13f28
Carl-J-M <carlmensahmail@gmail.com>
2020-07-26 15:40:33 +0100

    Added error message display on validation errors.

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -6,10 +6,12 @@
 
 <form method="POST" action="/task">
 
-    <div class="form-group">
-        <textarea name="description" class="form-control"></textarea>
-    </div>
-
+  <div class="form-group">
+          <textarea name="description" class="form-control"></textarea>
+          @if ($errors->has('description'))
+              <span class="text-danger">{{ $errors->first('description') }}</span>
+          @endif
+      </div>
 
     <div class="form-group">
         <button type="submit" class="btn btn-primary">Add Task</button>
